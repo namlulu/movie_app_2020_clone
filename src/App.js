2de38ef154402576c347e62c0eca0022ddaf029b
Namlulu <60912642+namlulu@users.noreply.github.com>
2020-09-04 00:40:23 +0900

    rendering 까지

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
 import React from 'react';
-import Proptypes from 'prop-types';
+import axios from 'axios';
+import Movie from './Movie';
 
 class App extends React.Component{
   state = {
     isLoading: true,
     movies: []
   };
+  
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    this.setState({ movies, isLoading: false })
+    //하나는 setstate: 하나는 axio에서 온 데이터 무비
+  }
+
   componentDidMount(){
-    setTimeout(()=>{
-      this.setState({isLoading: false})
-    }, 6000)
+    this.getMovies();    
   };
+
   render(){
-    const { isLoading } = this.state;
-    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
+    const { isLoading, movies } = this.state;
+    return <div>{isLoading ? "Loading..." : movies.map(movie => {
+      console.log(movie);
+      return <Movie 
+        id={movie.year} 
+        year={movie.year} 
+        title={movie.title} 
+        summary={movie.summary} 
+        poster={movie.medium_cover_image}/>
+    })}
+  </div>;
   }
   
 }
