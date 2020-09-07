742826cf11d469f733d1bc519c460c78a69f3309
Namlulu <60912642+namlulu@users.noreply.github.com>
2020-09-08 02:21:51 +0900

    finish, make movie list

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import axios from 'axios';
 import Movie from './Movie';
+import "./App.css";
 
-class App extends React.Component{
+class App extends React.Component {
   state = {
     isLoading: true,
     movies: []
@@ -24,18 +25,31 @@ class App extends React.Component{
 
   render(){
     const { isLoading, movies } = this.state;
-    return <div>{isLoading ? "Loading..." : movies.map(movie => {
-      console.log(movie);
-      return <Movie 
-        id={movie.year} 
-        year={movie.year} 
-        title={movie.title} 
-        summary={movie.summary} 
-        poster={movie.medium_cover_image}/>
-    })}
-  </div>;
-  }
-  
+    return (
+    <section className="container">
+        {isLoading ? (
+            <div className="loader">
+                <span className="loader__text">Loading...</span>
+            </div>
+        ) : (
+            <div className="movies">
+              {movies.map(movie => (
+                <Movie 
+                    key={movie.id}
+                    id={movie.year} 
+                    year={movie.year} 
+                    title={movie.title} 
+                    summary={movie.summary} 
+                    poster={movie.medium_cover_image}
+                    genres={movie.genres}
+                />
+            ))}
+            </div>
+        )}
+    </section>
+    );
+  };
+ 
 }
 
 export default App;
