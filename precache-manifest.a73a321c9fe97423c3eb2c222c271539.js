721bcea717f724a45cf5f076bcfb36ffe0d6e086
Namlulu <60912642+namlulu@users.noreply.github.com>
2020-09-08 23:56:33 +0900

    Updates

---
+++ precache-manifest.a73a321c9fe97423c3eb2c222c271539.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "612663f614b129e54f8bf467ce90cf77",
+    "url": "/movie_app_2020_clone/index.html"
+  },
+  {
+    "revision": "0727deb62428ecf127f1",
+    "url": "/movie_app_2020_clone/static/css/main.ad93ebe7.chunk.css"
+  },
+  {
+    "revision": "a7d22b2c00d8d326c863",
+    "url": "/movie_app_2020_clone/static/js/2.300fc6d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020_clone/static/js/2.300fc6d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0727deb62428ecf127f1",
+    "url": "/movie_app_2020_clone/static/js/main.de179849.chunk.js"
+  },
+  {
+    "revision": "997fa410a8a2691c29bf",
+    "url": "/movie_app_2020_clone/static/js/runtime-main.cc7dc4e9.js"
+  }
+]);
