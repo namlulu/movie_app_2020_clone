e43ee1d4c8db213b0e3381b9ef75872327f29db0
Namlulu <60912642+namlulu@users.noreply.github.com>
2020-09-09 15:53:07 +0900

    finish

--- src/App.js
+++ src/App.js
@@ -1,55 +1,21 @@
 import React from 'react';
-import axios from 'axios';
-import Movie from './Movie';
+import { HashRouter, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Detail from "./routes/Detail";
+import Navigation from "./components/Navigation";
 import "./App.css";
 
-class App extends React.Component {
-  state = {
-    isLoading: true,
-    movies: []
-  };
-  
-  getMovies = async () => {
-    const {
-      data: {
-        data: { movies }
-      }
-    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
-    this.setState({ movies, isLoading: false })
-    //하나는 setstate: 하나는 axio에서 온 데이터 무비
-  }
 
-  componentDidMount(){
-    this.getMovies();    
-  };
-
-  render(){
-    const { isLoading, movies } = this.state;
+function App() {
     return (
-    <section className="container">
-        {isLoading ? (
-            <div className="loader">
-                <span className="loader__text">Loading...</span>
-            </div>
-        ) : (
-            <div className="movies">
-              {movies.map(movie => (
-                <Movie 
-                    key={movie.id}
-                    id={movie.year} 
-                    year={movie.year} 
-                    title={movie.title} 
-                    summary={movie.summary} 
-                    poster={movie.medium_cover_image}
-                    genres={movie.genres}
-                />
-            ))}
-            </div>
-        )}
-    </section>
+    <HashRouter>
+        <Navigation></Navigation>
+        <Route path="/" exact={true} component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/movie/:id" component={Detail} />
+    </HashRouter>
     );
-  };
- 
 }
 
 export default App;
---
+++ src/routes/About.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function About(){
+    return <div className="about__container">About this page</div>
+}
+
+export default About;
